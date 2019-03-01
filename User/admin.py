acb8659f9216442726ad4d3fcff75d2575a85784
declercq sebastien <sdq@afnor.fr>
2019-03-01 17:00:48 +0100

    User: development started, not fully working so far

--- User/admin.py
+++ User/admin.py
@@ -1,3 +1,76 @@
+from typing import Any, Dict, Optional, Sequence, Tuple, Type
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from .models import CustomUser
 
-# Register your models here.
+
+Fieldset = Tuple[Optional[str], Dict[str, Sequence[str]]]
+
+
+class UserCreationForm(forms.ModelForm):
+    '''A from for creating new Users'''
+    password1: forms.CharField = forms.CharField(
+        label='Password', widget=forms.PasswordInput
+    )
+    password2: forms.CharField = forms.CharField(
+        label='Password', widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model: Type[CustomUser] = CustomUser
+        fields: Sequence[str] = ('email', 'name', 'firstname',)
+
+    def clean_password2(self) -> Optional[str]:
+        password1: Optional[str] = self.cleaned_data.get('password1')
+        password2: Optional[str] = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Password mismatch')
+        return password2
+
+    def save(self, commit: bool = True) -> Any:
+        user: Any = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password1'))
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password: ReadOnlyPasswordHashField = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model: Type[CustomUser] = CustomUser
+        fields = ('email', 'password', 'name', 'firstname',
+                  'is_active', 'is_admin')
+
+    def clean_password(self) -> Optional[str]:
+        return self.initial.get('password')
+
+
+class UserAdmin(BaseUserAdmin):
+    form: Type[UserChangeForm] = UserChangeForm
+    add_form: Type[UserCreationForm] = UserCreationForm
+
+    list_display: Sequence[str] = ('email', 'name', 'firstname', 'is_admin')
+    list_filter: Sequence[str] = ('is_admin',)
+    fieldsets: Tuple[Fieldset, Fieldset, Fieldset] = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name', 'firstname')}),
+        ('Permissions', {'fields': ('is_admin',)})
+    )
+    add_fieldsets: Tuple[Fieldset] = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'firstname', 'password1', 'password2')
+        }),
+    )
+    search_fields: Sequence[str] = ('email', 'name')
+    ordering: Sequence[str] = ('name', 'firstname')
+    filter_horizontal: Sequence[str] = ()
+
+
+admin.site.register(CustomUser, UserAdmin)
+admin.site.unregister(Group)
--- User/models.py
+++ User/models.py
@@ -1,3 +1,65 @@
+from typing import Any, List, Optional, Sequence
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    '''Custom Manager overriding basic behavior from default UserManager.'''
+    def create_user(self, *args: Any, **kwargs: Any) -> Any:
+        '''Creates and saves a new User w/ email and password'''
+        user: Any = self._set_user(*args, **kwargs)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, *args: Any, **kwargs: Any) -> Any:
+        '''A superuser is a classic user'''
+        user: Any = self._set_user(*args, **kwargs)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+    def _set_user(
+        self, email: str, password: str,
+        name: Optional[str] = None, firstname: Optional[str] = None
+    ) -> Any:
+        '''Private method generating a User'''
+        required_fields: Sequence[str] = (
+            CustomUser.USERNAME_FIELD, *CustomUser.REQUIRED_FIELDS
+        )
+        for required_field in required_fields:
+            if not required_field:
+                raise ValueError(f'A {required_field} is required for Users')
+        user: CustomUser = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            firstname=firstname
+        )
+        user.set_password(password)
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+    '''Custom User Model'''
+    email: models.EmailField = models.EmailField(max_length=255, unique=True)
+    name: models.CharField = models.CharField(max_length=255, null=True)
+    firstname: models.CharField = models.CharField(max_length=255, null=True)
+    is_active: models.BooleanField = models.BooleanField(default=True)
+    is_admin: models.BooleanField = models.BooleanField(default=False)
+
+    objects: CustomUserManager = CustomUserManager()
+
+    USERNAME_FIELD: str = 'email'
+    REQUIRED_FIELDS: List[str] = ['password']
+
+    def __str__(self) -> str:
+        return self.email
+
+    def has_perm(self, perm: str, obj: Any = None) -> bool:
+        return True
+
+    def has_module_perms(self, app_label: str) -> bool:
+        return True
+
+    @property
+    def is_staff(self) -> bool:
+        return self.is_admin

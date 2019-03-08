65eda2e7702e1bcd6bfeede11739dd04846a68f6
declercq sebastien <sdq@afnor.fr>
2019-03-08 14:33:30 +0100

    User administration OK

--- User/admin.py
+++ User/admin.py
@@ -1,3 +1,14 @@
+from typing import Sequence, Type
 from django.contrib import admin
+from .models import User
+from .forms import AdminForm
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    form: Type[AdminForm] = AdminForm
+    list_display: Sequence[str] = ('email', 'name', 'firstname', 'is_active',
+                                   'is_staff',)
+    list_filter: Sequence[str] = ('is_active', 'is_staff', 'is_superuser',)
+    search_fields: Sequence[str] = ('email', 'name',)
+    ordering: Sequence[str] = ('email', 'name', 'firstname',)
---
+++ User/forms.py
@@ -0,0 +1,33 @@
+from typing import Optional, Sequence, Type
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from .models import User
+
+
+class AdminForm(forms.ModelForm):
+    # NOTE: For a more dynamic form (w/ JS), refer to :
+    # https://stackoverflow.com/a/15992088
+    password1 = forms.CharField(label=_('Password'),
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_('Password confirmation'),
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model: Type[User] = User
+        fields: Sequence[str] = ('email', 'password1', 'password2', 'name',
+                                 'firstname', 'is_active', 'is_superuser',
+                                 'is_staff', 'user_permissions',)
+
+    def clean_password2(self) -> Optional[str]:
+        password1: Optional[str] = self.cleaned_data.get('password1')
+        password2: Optional[str] = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Password mismatch')
+        return password2
+
+    def save(self, commit: bool = True) -> User:
+        user: User = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password1'))
+        if commit:
+            user.save()
+        return user

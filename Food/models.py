c98b74fac791309d55434a135650b47f829a33ff
declercq sebastien <sdq@afnor.fr>
2019-03-15 11:34:51 +0100

    Food: 1st commit (some tests + minimal Product model)

--- Food/models.py
+++ Food/models.py
@@ -1,3 +1,36 @@
+from typing import Sequence, Tuple
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Product(models.Model):
+    NUTRITION_GRADES: Sequence[Tuple[str, str]] = (
+        ('A', 'A'),
+        ('B', 'B'),
+        ('C', 'C'),
+        ('D', 'D'),
+        ('E', 'E'),
+    )
+
+    barcode: models.CharField = models.CharField(
+        _('barcode'), max_length=48, blank=False, unique=True
+    )
+    name: models.CharField = models.CharField(
+        _('product name'), max_length=500, blank=False
+    )
+    nutrition_grade: models.CharField = models.CharField(
+        _('nutrition_grade'), choices=NUTRITION_GRADES, blank=False,
+        max_length=1
+    )
+    url: models.URLField = models.URLField(blank=False, unique=True)
+
+    objects: models.Manager = models.Manager()
+
+    class Meta:
+        ordering: Sequence[str] = ('name', 'nutrition_grade',)
+        verbose_name: str = _('product')
+        verbose_name_plural: str = _('products')
+
+    def __str__(self) -> str:
+        return (f'<Product#{self.barcode} name={self.name} '
+                f'nutrition_grade={self.nutrition_grade}')
--- Food/tests.py
+++ Food/tests.py
@@ -1,3 +1,25 @@
+from typing import Dict, Optional
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+
+class TestProductModel(TestCase):
+    def setUp(self) -> None:
+        self.data: Dict[str, str] = dict(
+            barcode='123456', name='Test product',
+            nutrition_grade='A', url='http://example.com',
+        )
+
+    def test_product_insertion(self) -> None:
+        product: Product = Product.objects.create(**self.data)
+        self.assertEqual(Product.objects.first(), product)
+
+    def test_product_repr(self) -> None:
+        Product.objects.create(**self.data)
+        product: Optional[Product] = Product.objects.first()
+        if product is not None:
+            self.assertEqual(
+                str(product),
+                f'<Product#{self.data["barcode"]} name={self.data["name"]} '
+                f'nutrition_grade={self.data["nutrition_grade"]}'
+            )

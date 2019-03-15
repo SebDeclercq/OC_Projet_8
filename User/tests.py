dcfb7eb0a36f73a634c623e052c675aa80e76335
declercq sebastien <sdq@afnor.fr>
2019-03-15 10:52:48 +0100

    User: almost 100% test coverage

--- User/tests.py
+++ User/tests.py
@@ -1,12 +1,12 @@
-from typing import Any
-from django.http import HttpRequest, HttpResponse
+from typing import Dict, Optional
+from django.http import HttpResponse
 from django.test import Client, TestCase
 from django.urls import resolve
 from .models import User
 from .views import LoginView
 
 
-class LogInTest(TestCase):
+class LogInViewTest(TestCase):
     URL: str = '/user/login'
 
     def setUp(self) -> None:
@@ -15,7 +15,6 @@ class LogInTest(TestCase):
         self.user: User = User.objects.create_user(
             email='az@er.ty', password=self.valid_password
         )
-        print(self.user.password)
 
     def test_url_resolve_login(self) -> None:
         self.assertEqual(
@@ -24,10 +23,7 @@ class LogInTest(TestCase):
 
     def test_page_return_expected_html(self) -> None:
         response: HttpResponse = self.client.get(self.URL)
-        html: str = response.content.decode('utf8').strip('\n')
-        self.assertTrue(html.startswith('<!DOCTYPE'))
-        self.assertIn('<title>Log in</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTemplateUsed(response, 'login.html')
 
     def test_login_valid_credentials(self) -> None:
         response: HttpResponse = self.client.post(self.URL, {
@@ -42,3 +38,84 @@ class LogInTest(TestCase):
         })
         html: str = response.content.decode('utf8')
         self.assertIn('WRONG CREDENTIALS, PLEASE RETRY', html)
+
+
+class SignUpModelTest(TestCase):
+    def test_user_manage_create_user(self) -> None:
+        user: User = User.objects.create_user(
+            email='az@er.ty', password='azerty',
+            name='Uiop', firstname='Azerty'
+        )
+        inserted_user: Optional[User] = User.objects.first()
+        if inserted_user is not None:
+            self.assertEqual(inserted_user, user)
+            self.assertEqual(inserted_user.get_full_name(),
+                             'Azerty Uiop')
+
+    def test_user_manager_create_superuser(self) -> None:
+        superuser: User = User.objects.create_superuser(
+            email='az@er.ty', password='azerty'
+        )
+        self.assertEqual(User.objects.first(), superuser)
+
+    def test_user_manager_create_superuser_attrs(self) -> None:
+        User.objects.create_superuser(
+            email='az@er.ty', password='azerty'
+        )
+        superuser: Optional[User] = User.objects.first()
+        if superuser is not None:
+            self.assertEqual(superuser.is_superuser, True)
+            self.assertEqual(superuser.is_staff, True)
+
+
+class SignUpViewTest(TestCase):
+    URL: str = '/user/signup'
+    LOGIN_URL: str = 'login'
+
+    def setUp(self) -> None:
+        self.client: Client = Client()
+        self.data: Dict[str, str] = {
+            'password1': 'azerty',
+            'password2': 'azerty',
+            'email': 'az@er.ty'
+        }
+
+    def test_page_return_expected_html(self) -> None:
+        response: HttpResponse = self.client.get(self.URL)
+        self.assertTemplateUsed(response, 'signup.html')
+
+    def test_new_user_sign_up(self) -> None:
+        response: HttpResponse = self.client.post(self.URL, data=self.data,
+                                                  follow=True)
+        self.assertEqual(response.redirect_chain[0], (self.LOGIN_URL, 302))
+
+    def test_new_user_sign_up_insertion(self) -> None:
+        self.client.post(self.URL, data=self.data)
+        user: Optional[User] = User.objects.first()
+        if user is not None:
+            self.assertEqual(user.email, self.data['email'])
+            self.assertTrue(user.check_password(self.data['password1']))
+
+    def test_bad_form_completion(self) -> None:
+        data: Dict[str, str] = self.data.copy()
+        data['password2'] = 'xxx'
+        r: HttpResponse = self.client.post(self.URL, data=data)
+        self.assertIn('Password mismatch', r.content.decode('utf-8'))
+
+
+class LogOutViewTest(TestCase):
+    URL: str = '/user/logout'
+    LOGIN_URL: str = '/user/login'
+
+    def setUp(self) -> None:
+        self.client: Client = Client()
+        user: User = User.objects.create_user(
+            email='az@er.ty', password='azerty'
+        )
+        self.client.post(self.LOGIN_URL, {
+            'email': user.email, 'password': 'azerty'
+        })
+
+    def test_logout(self) -> None:
+        response: HttpResponse = self.client.get(self.URL, follow=True)
+        self.assertEqual(response.redirect_chain[0], (self.LOGIN_URL, 302))

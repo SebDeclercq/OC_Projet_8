436dbd3960a78b6ec2bab100b602e529fc97335a
declercq sebastien <sdq@afnor.fr>
2019-05-03 10:33:14 +0200

    User: User insertion is now covered by one test for models.py

--- User/tests.py
+++ User/tests.py
@@ -7,6 +7,21 @@ from .models import User
 from .views import LoginView
 
 
+class UserModel(TestCase):
+    def test_creation(self) -> None:
+        email: str = 'az@er.ty'
+        password: str = 'azerty'
+        firstname: str = 'Azerty'
+        user: User = User.objects.create_user(
+            email=email, password=password, firstname=firstname
+        )
+        inserted_user: User = User.objects.first()  # type: ignore
+        self.assertEqual(user, inserted_user)
+        self.assertEqual(email, inserted_user.email)
+        self.assertTrue(inserted_user.check_password(password))
+        self.assertEqual(firstname, inserted_user.firstname)
+
+
 class LogInViewTest(TestCase):
     URL: str = '/user/login'
 
